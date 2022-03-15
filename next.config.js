fd5f84945e2af54d66104d23febab0dffb769eb4
Jeff Free <jefffreelance3@gmail.com>
2022-03-15 02:59:56 -0300

    :rocket: deploying to vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-module.exports = {
+const withPwa = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPwa({
+	pwa: {
+		dest: 'public',
+		register: true,
+		mode: 'production',
+		disable: false,
+		runtimeCaching,
+		buildExcludes: [/middleware-manifest\.json$/],
+	},
 	reactStrictMode: true,
 	images: {
 		domains: ['placeimg.com'],
 	},
-};
+});
